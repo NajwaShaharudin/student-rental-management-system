4283cf73996dbd6d8a7955a98b84d5fd4aad79f8
najwa <najwashaharudinoj@gmail.com>
2023-12-18 13:59:56 +0800

    configure admin login page

--- student-rental-management-system/admin_login.php
+++ student-rental-management-system/admin_login.php
@@ -55,12 +55,33 @@
     <section class="section" id="contact-us" style="margin-top: 0">
         <div class="container">
 
-       
-           
+        <?php
+          if (isset($_POST["login"])) {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+             require_once "database.php";
+             $sql = "SELECT * FROM admin_login WHERE username = '$username'";
+             $result = mysqli_query($conn, $sql);
+             $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+             if ($user) {
+                 if (password_verify($password, $user["password"])) {
+                     session_start();
+                     $_SESSION["user"] = "yes";
+                     header("Location: index.php");
+                     die();
+                 }else{
+                     echo "<div class='alert alert-danger'>Password does not match</div>";
+                 }
+             }else{
+                 echo "<div class='alert alert-danger'>Username does not match</div>";
+             }
+         }
+        ?>
+
                     <div class="contact-form section-bg" style="background-image: url(assets/images/contact-1-720x480.jpg)">
-                        <form id="admin" action="admin.php" method="post">
+                        <form id="admin" action="admin_login.php" method="post">
                               <fieldset>
-                              <input type="username" placeholder="Enter Username:" name="username" class="form-control">
+                              <input type="text" placeholder="Enter Username:" name="username" class="form-control">
                               </fieldset>
                               <fieldset>
                               <input type="password" placeholder="Enter Password:" name="password" class="form-control">
@@ -81,7 +102,6 @@
                 <div class="col-lg-12">
                     <p>
                         Copyright © student-utem
-                        
                     </p>
                 </div>
             </div>
