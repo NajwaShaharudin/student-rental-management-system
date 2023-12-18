ab80e33a80a05e22b28e7d1b26b2776de14362d4
najwa <najwashaharudinoj@gmail.com>
2023-12-18 15:07:32 +0800

    add nav bar

--- student-rental-management-system/login.php
+++ student-rental-management-system/login.php
@@ -33,6 +33,30 @@
       </div>
     </div>
     <!-- ***** Preloader End ***** -->
+
+     <!-- ***** Header Area Start ***** -->
+     <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.php" class="logo">House<em> Rental</em></a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="index.php">Home</a></li>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
     
 
     <!-- ***** Call to Action Start ***** -->
